41a7f570249c6d2e157f4ab17af0f663bc3173f3
alessandrokonrad <akonrad6@gmail.com>
2023-04-05 12:18:14 +0200

    Bump

--- contract/build.ts
+++ contract/build.ts
@@ -25,9 +25,9 @@ await build({
     type: "module",
   },
   mappings: {
-    "https://deno.land/x/lucid@0.10.0/mod.ts": {
+    "https://deno.land/x/lucid@0.10.1/mod.ts": {
       name: "lucid-cardano",
-      version: ">=0.9.4 <=0.10.0",
+      version: ">=0.9.4 <=0.10.1",
       peerDependency: true,
     },
   },
